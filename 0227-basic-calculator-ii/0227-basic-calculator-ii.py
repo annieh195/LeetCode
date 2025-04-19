694e158227478c4506fa18dedfbc8f11d524b4a6
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-04-18 18:03:55 -0700

    Time: 99 ms (25.89%), Space: 21.6 MB (51.19%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,25 +1,36 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        def check(op, stack, cur):
-            if op == '-':
-                return stack.append(-cur)
-            elif op == '+':
-                return stack.append(cur)
-            elif op == '*':
-                return stack.append(stack.pop() * cur)
-            elif op == '/':
-                return stack.append(int(stack.pop() / cur))
-            
+        s = ''.join(s.split())
         stack = []
-        ops = "+-*/"
-        op = "+"
-        cur = 0
+        i = 0
+        j = 0
 
-        for char in s + '+':
-            if char.isdigit():
-                cur = (cur * 10) + int(char)
-            elif char in ops: 
-                check(op, stack, cur)
-                cur = 0 
-                op = char
-        return sum(stack)
+        def get_num(i):
+            j = i + 1
+            a = 0
+            while j < len(s) and s[j].isdigit():
+                j += 1
+            stack.append(int(s[i:j]))
+            return j
+
+        while i < len(s):
+            if s[i] == '+':
+                i += 1
+                continue 
+            if s[i].isdigit() or s[i] == '-':
+                i = get_num(i)
+            else:
+                j = get_num(i+1)
+                num1, num2 = stack.pop(), stack.pop()
+                if s[i] == "*":
+                    stack.append(num2*num1)
+                elif s[i] == "/":
+                    # print(num2, num1, num2/num1)
+                    a = abs(num2)//abs(num1)
+                    if num2/num1 < 0:
+                        a *= -1
+                    stack.append(a)
+                    # print(stack)
+                i = j
+      
+        return int(sum(stack))
