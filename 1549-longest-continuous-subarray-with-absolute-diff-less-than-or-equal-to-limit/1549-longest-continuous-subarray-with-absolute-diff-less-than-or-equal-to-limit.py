684d0ceff11841f019a78b5b07e9dcdefcbac448
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-05-01 15:02:55 -0700

    Time: 824 ms (6.23%), Space: 26.6 MB (49.68%) - LeetHub

---
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        sl = SortedList()
+        res = 0
+        l = 0
+        
+        for r in range(len(nums)):
+            sl.add(nums[r])
+            while l <= r and abs(sl[-1]-sl[0]) > limit:
+                sl.discard(nums[l])  
+                l += 1
+            res = max(res, len(sl))
+        return res
