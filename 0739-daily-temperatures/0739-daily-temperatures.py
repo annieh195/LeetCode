8efa015179da663438236ba0ccce96a48fc2ebcd
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-04-30 18:12:21 -0700

    Time: 99 ms (63.28%), Space: 27.2 MB (76.91%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,8 +1,7 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        temperatures.append(-inf)
         st = []
-        res = [0]*(len(temperatures)-1)
+        res = [0]*len(temperatures)
 
         for i in range(len(temperatures)):
             while st and temperatures[st[-1]] < temperatures[i]:
