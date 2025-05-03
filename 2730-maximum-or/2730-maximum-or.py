4108512bf7c7151fe3bb481f9009903311e2e527
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-05-02 18:13:13 -0700

    Time: 163 ms (28.06%), Space: 33.7 MB (25.18%) - LeetHub

--- 2730-maximum-or/2730-maximum-or.py
+++ 2730-maximum-or/2730-maximum-or.py
@@ -1,24 +1,24 @@
 class Solution:
     def maximumOr(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        preSum = [0]*n
-        surSum = [0]*n
-        surSum[-1] = nums[-1]
-        preSum[0] = nums[0]
+        preOr = [0]*n
+        surOr = [0]*n
+        surOr[-1] = nums[-1]
+        preOr[0] = nums[0]
 
         for i in range(1, n):
-            preSum[i] = preSum[i-1] | nums[i]
+            preOr[i] = preOr[i-1] | nums[i]
         
         for i in range(n-2, -1, -1):
-            surSum[i] = surSum[i+1] | nums[i]
+            surOr[i] = surOr[i+1] | nums[i]
 
         maxx = 0
         for i in range(n):
             l = r = 0
             if i-1 >= 0:
-                l = preSum[i-1]
+                l = preOr[i-1]
             if i+1 < n:
-                r = surSum[i+1]
+                r = surOr[i+1]
     
             maxx = max(maxx, l | nums[i]<<k | r)
         return maxx
