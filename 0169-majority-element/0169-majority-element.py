567cf34340bad865e65a24c893cc4933cd95306c
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-06-09 16:00:38 -0700

    Time: 11 ms (29.62%), Space: 19.6 MB (5.15%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+        for num in nums:
+            mp[num] += 1
+        
+        for num in nums:
+            if mp[num] > (len(nums) / 2):
+                return num
