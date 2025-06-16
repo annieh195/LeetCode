40de431e77d89255e9bb0fb576a52981875846be
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-06-16 10:41:34 -0700

    Time: 7 ms (3%), Space: 18.2 MB (8.67%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        res = 0
+        countL = 0
+        countR = 0
+
+        for i in range(len(s)):
+            if countL == countR and countL >= 1 and countR >= 1:
+                res += 1
+                countL = 0
+                countR = 0
+                print(countL, countR, res)
+            if s[i] == 'R':
+                countR += 1
+                print('R', i, countR)
+            if s[i] == 'L':
+                countL += 1
+                print('L', i, countL)
+        return res+1
