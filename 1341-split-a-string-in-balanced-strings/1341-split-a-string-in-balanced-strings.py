85ef59ec9592bfbda1181a5e95d3b1d53e47331f
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-06-16 10:44:46 -0700

    Time: 1 ms (19.07%), Space: 17.7 MB (78.33%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -7,8 +7,6 @@ class Solution:
         for i in range(len(s)):
             if countL == countR and countL >= 1 and countR >= 1:
                 res += 1
-                countL = 0
-                countR = 0
             if s[i] == 'R':
                 countR += 1
             if s[i] == 'L':
