d6484874d8db3dc150586b20a01e93a34dfcdfc7
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-07-09 14:49:01 -0700

    Time: 27 ms (49.5%), Space: 19.5 MB (60.41%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        newnums = nums + nums + [inf]
+        new_nums = nums + nums + [inf]
+        res = [-1]*len(nums)
         st = []
-        res = [-1] * len(nums)
 
-        for i in range(len(newnums)):
-            while st and newnums[st[-1]] < newnums[i]:
+        for i in range(len(new_nums)):
+            while st and new_nums[st[-1]] < new_nums[i]:
                 j = st.pop()
-                if j < len(nums) and newnums[i] != inf:
-                    res[j] = newnums[i]
+                if j < len(nums) and new_nums[i] != inf:
+                    res[j] = new_nums[i]
             st.append(i)
         return res
