becb3a27435755b73661437aee8bb6ab51c5069f
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-07-22 17:47:52 -0700

    Time: 157 ms (45.05%), Space: 18.9 MB (7.52%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,39 +1,19 @@
 class MyCalendar:
 
     def __init__(self):
-        self.calendar = SortedList()
+        self.startTimes = SortedList()
+        self.endTimes = SortedList()
 
     def book(self, startTime: int, endTime: int) -> bool:
-        if len(self.calendar) == 0:
-            self.calendar.add([startTime, endTime])
+        left = bisect_left(self.startTimes, endTime)
+        right = bisect_right(self.endTimes, startTime)
+        if left == right:
+            self.startTimes.add(startTime)
+            self.endTimes.add(endTime)
             return True
         else:
-            for i in range(len(self.calendar)):
-                curr_start, curr_end = self.calendar[i]
+            return False
 
-                if endTime <= curr_start:
-                    if i > 0 and self.calendar[i-1][1] == startTime:
-                        startTime = self.calendar[i-1][0]
-                        self.calendar.discard(self.calendar[i-1])
-                    if startTime == curr_start:
-                        endTime = curr_end
-                        self.calendar.discard(self.calendar[i])
-                    self.calendar.add([startTime, endTime])
-                    return True
-
-                elif startTime >= curr_end:
-                    if i == len(self.calendar) - 1:
-                        if curr_end == startTime:
-                            startTime = curr_start
-                            self.calendar.discard(self.calendar[i])
-                        self.calendar.add([startTime, endTime])
-                        return True
-                    continue
-                else:
-                    return False
-
-            self.calendar.add([startTime, endTime])
-            return True
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
