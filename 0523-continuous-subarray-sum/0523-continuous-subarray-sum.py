1e5cd0a4486ee53ecad1af6a25c1a64bb08aeeb3
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-09-13 17:27:45 -0700

    Time: 107 ms (6.93%), Space: 41.3 MB (5.03%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,20 +1,16 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         prefix = [0]*(len(nums)+1)
-        
+
         for i in range(1, len(nums)+1):
             prefix[i] = prefix[i-1] + nums[i-1]
-
-        mp = {}
+        
+        mp = defaultdict(int)
         for r in range(len(prefix)):
-            if k != 0:
-                mod = prefix[r] % k 
-            else:
-                mod = prefix[r]
-                
-            if mod in mp:
-                if r - mp[mod] >= 2:
+            mod = prefix[r]%k
+            if mod in mp: 
+                if r-mp[mod] >= 2:
                     return True
             else:
                 mp[mod] = r
-        return False
+        return False    
