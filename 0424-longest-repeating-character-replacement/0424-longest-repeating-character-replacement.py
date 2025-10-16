87182a65a52aa5283852dae54badb2dbe0e42ece
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2025-10-15 19:44:30 -0700

    Time: 179 ms (26.74%), Space: 18.1 MB (23.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,7 +6,7 @@ class Solution:
 
         for r in range(len(s)):
             window[s[r]] += 1
-            while (r - l + 1) - max(window.values()) > k:
+            while (r-l+1) - max(window.values()) > k:
                 window[s[l]] -= 1
                 l += 1
             res = max(res, r-l+1)
