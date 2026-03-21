14b6f239b9d1bd8d11f8df3c523b0b3f6a1230c4
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2026-03-20 22:27:59 -0700

    Time: 559 ms (14.89%), Space: 19.3 MB (77.79%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,29 +1,23 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        l1 = 0
-        r1 = len(nums)-1
         nums.sort()
-        res = []
+        n = len(nums)
+        res = set()
 
-        for l1 in range(len(nums)):
-            if l1 > 0 and nums[l1] == nums[l1-1]:
-                continue
-            for r1 in range(len(nums)-1, l1+2, -1):
-                if r1 < len(nums)-1 and nums[r1] == nums[r1+1]:
-                    continue
+        for i in range(n-3):
+            for j in range(i+1, n-2): 
+                
+                l = j+1
+                r = n-1
+                while l < r:
+                    total = nums[l] + nums[r] + nums[i] + nums[j]
 
-                diff1 = target - (nums[l1] + nums[r1])
-                l2 = l1+1
-                r2 = r1-1
-                while l2 < r2:
-                    diff2 = nums[l2] + nums[r2]
-                    if diff1 == diff2:
-                        if [nums[l1], nums[l2], nums[r2], nums[r1]] not in res:
-                            res.append([nums[l1], nums[l2], nums[r2], nums[r1]])
-                        l2 += 1
-                        r2 -= 1
-                    elif diff2 < diff1:
-                        l2 += 1
-                    else:
-                        r2 -= 1
-        return res
+                    if total == target:
+                        res.add((nums[l], nums[r], nums[i], nums[j]))
+                        l += 1
+                        r -= 1
+                    elif total < target:
+                        l += 1
+                    else: 
+                        r -= 1
+        return list(res)
