0fbecbc392937ad00d8c2406e0fb16bbd1955e62
Anh Huynh <102700415+annieh195@users.noreply.github.com>
2026-04-03 11:15:09 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,11 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        check = []
-        mapParentheses = {")":"(", "}":"{", "]":"["}
+        openBrackets = ['(', '[', '{']
+        stack = []
+
+        if len(s) < 2:
+            return False
+
         for char in s:
-            if char in mapParentheses.values():
-                check.append(char)
-            elif char in mapParentheses.keys():
-                if not check or mapParentheses[char] != check.pop():
-                    return False
-        return not check
+            if char in openBrackets:
+                stack.append(char)
+            elif stack and ((stack[-1] == '(' and char == ')') or (stack[-1] == '[' and char == ']') or (stack[-1] == '{' and char == '}')):
+                stack.pop()
+        
+        if stack:
+            return False
+        else:
+            return True
